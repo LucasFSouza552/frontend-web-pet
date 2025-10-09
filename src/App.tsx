ed78a854f0a2b879133354602bd58fcf377152dd
LucasFSouza552 <lucasfelipe552@hotmail.com>
2025-10-09 00:53:08 -0300

    feat: add ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import NavigationRouter from './shared/utils/NavigationRouter'
 import HomeSection from './features/home/pages/HomePage'
 
-import { ThemeProvider } from "styled-components";
 import { Route } from 'react-router-dom';
 
-import GlobalStyle from './shared/styles/GlobalStyle';
-import { lightTheme, darkTheme } from './shared/styles/Theme';
 import LoginSection from './features/auth/pages/LoginPage';
 import ProfileSection from './features/profile/pages/ProfilePage';
 import MatchSection from './features/match/pages/matchPage';
 import RegisterSection from './features/auth/pages/RegisterPage';
-import CommunityPage from './sections/Home/CommunityPage';
+import CommunityPage from './features/home/components/CommunityPage';
+import { ThemeProvider } from './app/contexts/ThemeContext';
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle theme={lightTheme} />
+    <ThemeProvider>
+
       <NavigationRouter>
         <Route path="/" element={<HomeSection />} />
         <Route path="/login" element={<LoginSection />} />
