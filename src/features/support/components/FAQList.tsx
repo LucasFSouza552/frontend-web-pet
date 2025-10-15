83fad13d3d6b008dd517e7bec49c64ac880af3c2
Thiago <thiago.goncalvesgames@gmail.com>
2025-10-14 21:25:10 -0300

    feat: refactor FAQList component to use dynamic rendering and improve layout

--- src/features/support/components/FAQList.tsx
+++ src/features/support/components/FAQList.tsx
@@ -1,47 +1,31 @@
 import { styled } from "styled-components";
 import Section from "../../../shared/styles/SectionStyle";
+import { useState } from "react";
+
+const faqs = [
+    { question: "Como funciona o processo de adoção?", answer: "Basta criar uma conta, escolher o animal que deseja adotar e preencher o formulário de interesse. Nossa equipe entrará em contato para validar as informações e agendar uma visita." },
+    { question: "Há algum custo para adotar?", answer: "Não! A adoção é gratuita. Porém, pedimos uma pequena contribuição opcional para ajudar a manter o projeto e cuidar de outros animais." },
+    { question: "Como posso ajudar sem adotar?", answer: "Você pode contribuir com doações de ração, medicamentos, itens de limpeza ou apoio financeiro. Toda ajuda faz diferença" },
+    { question: "Posso visitar os animais antes da adoção?", answer: "Sim! Agende uma visita presencial para conhecer os animais disponíveis e conversar com nossa equipe de voluntários." },
+    { question: "Como entro em contato com o suporte?", answer: "Você pode enviar uma mensagem pelo formulário abaixo ou entrar em contato via e-mail e WhatsApp. Nossa equipe responde em até 24h úteis." }
+];
 
 export default function FAQList() {
+    const [actualIndex, setActualIndex] = useState<number | null>(null);
+
     return (
         <Section height="auto">
-            <UnorderedList>
-                <ListItem>
-                    <ListItemTitle>1. Como funciona o processo de adoção?</ListItemTitle>
-                    <Paragraph>
-                        Basta criar uma conta, escolher o animal que deseja adotar e
-                        preencher o formulário de interesse. Nossa equipe entrará em
-                        contato para validar as informações e agendar uma visita.
-                    </Paragraph>
-                </ListItem>
-                <ListItem>
-                    <ListItemTitle>2. Há algum custo para adotar?</ListItemTitle>
-                    <Paragraph>
-                        Não! A adoção é gratuita. Porém, pedimos uma pequena contribuição opcional
-                        para ajudar a manter o projeto e cuidar de outros animais.
-                    </Paragraph>
-                </ListItem>
-                <ListItem>
-                    <ListItemTitle>3. Como posso ajudar sem adotar?</ListItemTitle>
-                    <Paragraph>
-                        Você pode contribuir com doações de ração, medicamentos,
-                        itens de limpeza ou apoio financeiro. Toda ajuda faz diferença
-                    </Paragraph>
-                </ListItem>
-                <ListItem>
-                    <ListItemTitle>4. Posso visitar os animais antes da adoção?</ListItemTitle>
-                    <Paragraph>
-                        Sim! Agende uma visita presencial para conhecer os animais
-                        disponíveis e conversar com nossa equipe de voluntários.
-                    </Paragraph>
-                </ListItem>
-                <ListItem>
-                    <ListItemTitle>5. Como entro em contato com o suporte?</ListItemTitle>
-                    <Paragraph>
-                        Você pode enviar uma mensagem pelo formulário abaixo
-                        ou entrar em contato via e-mail e WhatsApp. Nossa equipe responde em até 24h úteis.
-                    </Paragraph>
-                </ListItem>
-            </UnorderedList>
+            <FormContainer>
+                <UnorderedList>
+                    {faqs.map((faqs, index) => (
+                        <ListItem
+                            onClick={() => setActualIndex(actualIndex == index ? null : index)}>
+                            <ListItemTitle>{faqs.question}</ListItemTitle>
+                            {actualIndex == index && <Paragraph>{faqs.answer}</Paragraph>}
+                        </ListItem>
+                    ))}
+                </UnorderedList>
+            </FormContainer>
         </Section>
     );
 }
@@ -49,26 +33,34 @@ export default function FAQList() {
 const UnorderedList = styled.ul`
     display: flex;
     flex-direction: column;
-    background-color: #61475C;
+    max-width: 800px;
     color: white;
     align-items: center;
-    margin: 30px 205px;
-    border-radius: 50px;
-    padding: 10px;
-    text-align: center;
 `;
 
 const Paragraph = styled.p`
-    width: 700px;
-    padding-top: 20px
+    width: 500px;
+    background-color: #61475C;
+    border-radius: 0 0 15px 15px;
+    padding: 20px
+`;
+
+const FormContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 50px;
 `;
 
 const ListItem = styled.li`
-    padding: 20px;
+    padding: 5px;
 `;
 
 const ListItemTitle = styled.h3`
+    user-select: none;
+    width: 500px;
+    cursor: pointer;
     background-color: #B648A0;
-    border-radius: 5px;
-    padding: 5px;
+    border-radius: 15px 15px 0 0;
+    padding: 10px;
 `;
