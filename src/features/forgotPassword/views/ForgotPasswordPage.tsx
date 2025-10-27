c1d6256ee63b787c3b228c24ec5042ace46c6e28
Thiago <thiago.goncalvesgames@gmail.com>
2025-10-27 20:16:23 -0300

    feat: Rename Step2 component to ReceivedScreen and update references; add background image to ForgotPasswordPage

--- src/features/forgotPassword/views/ForgotPasswordPage.tsx
+++ src/features/forgotPassword/views/ForgotPasswordPage.tsx
@@ -2,7 +2,7 @@ import ErrorContainer from "../../../shared/components/ErrorContainer";
 import { HeaderComponent } from "../../../shared/components/HeaderComponent";
 import Section from "../../../shared/styles/SectionStyle";
 import { useForgotPasswordController } from "../../auth/controllers/useForgotPasswordController";
-import Step2 from "../components/Received";
+import ReceivedScreen from "../components/Received";
 import RecoveryPasswordForm from "../components/SendForm";
 import styled from "styled-components";
 
@@ -13,7 +13,7 @@ export default function ForgotPasswordPage() {
             <HeaderComponent />
             <ContentDiv>
                 {actualStep == 1 && <RecoveryPasswordForm email={email} handleChange={handleChange} handleSubmit={handleSubmit} />}
-                {actualStep == 2 && <Step2 />}
+                {actualStep == 2 && <ReceivedScreen />}
                 {error && <ErrorContainer message={error} />}
             </ContentDiv>
         </SectionForgotPassword>
@@ -23,6 +23,10 @@ export default function ForgotPasswordPage() {
 const SectionForgotPassword = styled(Section)`
     display: flex;
     align-items: center;
+    background-image: url("/Doodle pet 3.png");
+    background-size: 50%;
+    background-repeat: no-repeat;
+    background-position: bottom right;
 `;
 
 const ContentDiv = styled.div`
