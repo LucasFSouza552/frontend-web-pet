4e5253bb072b530a1d32cb9f14f3870ae3310649
Thiago Gonçalves <thiago.goncalvesgames@gmail.com>
2025-10-31 22:18:10 -0300

    feat: add historyService

---
+++ src/features/history/historyService.ts
@@ -0,0 +1,66 @@
+import api from "../../shared/api/Http";
+
+export const historyService = {
+    async fetchAllHistories() {
+        try {
+            const response = await api.get("/history");
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async fetchHistoryById(accountId: string) {
+        try {
+            const response = await api.get(`/history/:${accountId}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async createHistory(data: any) {
+        try {
+            const response = await api.post("/history/", data);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async updateHistory(historyId: string, data: any) {
+        try {
+            const response = await api.patch(`/history/${historyId}`, data);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async deleteHistory(historyId: string) {
+        try {
+            const response = await api.delete(`/history/${historyId}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async listHistoriesByAccount(accountId: string) {
+        try {
+            const response = await api.get(`/history/account/${accountId}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async updateHistoryStatus(historyId: string, status: string) {
+        try {
+            const response = await api.patch(`/history/status/${historyId}`, { status });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
